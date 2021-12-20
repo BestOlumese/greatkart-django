1428a431ce2bdb46df2de0efeab23c65747fabff
BestOlumese <bestolumese@gmail.com>
2021-12-20 14:41:12 +0100

    variation products complete

--- carts/migrations/0001_initial.py
+++ carts/migrations/0001_initial.py
@@ -16,7 +16,11 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Cart',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id',
+                 models.AutoField(auto_created=True,
+                                  primary_key=True,
+                                  serialize=False,
+                                  verbose_name='ID')),
                 ('cart_id', models.CharField(blank=True, max_length=250)),
                 ('date_added', models.DateTimeField(auto_now_add=True)),
             ],
@@ -24,11 +28,19 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='CartItem',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id',
+                 models.AutoField(auto_created=True,
+                                  primary_key=True,
+                                  serialize=False,
+                                  verbose_name='ID')),
                 ('quantity', models.IntegerField()),
                 ('is_active', models.BooleanField(default=True)),
-                ('cart', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='carts.cart')),
-                ('product', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='store.product')),
+                ('cart',
+                 models.ForeignKey(on_delete=django.db.models.deletion.CASCADE,
+                                   to='carts.cart')),
+                ('product',
+                 models.ForeignKey(on_delete=django.db.models.deletion.CASCADE,
+                                   to='store.product')),
             ],
         ),
     ]
