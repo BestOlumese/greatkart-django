5f17d2375d955bd533b0b96c404a6be59c7160e6
BestOlumese <bestolumese@gmail.com>
2021-12-26 20:18:09 +0100

    User Account Activation

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'accounts',
     'store',
     'carts',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
@@ -132,3 +133,16 @@ STATICFILES_DIRS = [
 # Media Files Config
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+
+# SMTP configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'bestolumese@gmail.com'
+EMAIL_HOST_PASSWORD = 'megemege1234$'
+EMAIL_USE_TLS = True
